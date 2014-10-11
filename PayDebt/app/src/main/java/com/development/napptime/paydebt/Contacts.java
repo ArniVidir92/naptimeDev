20453df3873418e520ce128ab71e84e39333af94
Arnividir92 <avj2@hi.is>
2014-10-11 15:02:07 +0000

    Laga navigation drawer med nyju projecti

---
+++ PayDebt/app/src/main/java/com/development/napptime/paydebt/Contacts.java
@@ -0,0 +1,19 @@
+package com.development.napptime.paydebt;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by arni on 10/11/14.
+ */
+public class Contacts extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.lay_contacts, container, false);
+        return rootView;
+    }
+}
---
+++ PayDebt/app/src/main/java/com/development/napptime/paydebt/Favorites.java
@@ -0,0 +1,19 @@
+package com.development.napptime.paydebt;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by arni on 10/11/14.
+ */
+public class Favorites extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.lay_favorites, container, false);
+        return rootView;
+    }
+}
